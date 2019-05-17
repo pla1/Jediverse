9358be30a9af207dbeba9147ad9459bd12d1cd4c
Patrick L Archibald <patrick.archibald@gmail.com>
2019-05-16 21:55:50 -0400

    Use current working directory for target folder.

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 java -cp "${HOME}/.m2/repository/com/google/code/gson/gson/2.8.5/*:\
 ${HOME}/.m2/repository/com/google/guava/guava/19.0/*:\
 ${HOME}/.m2/repository/org/jsoup/jsoup/1.11.3/*:\
-${HOME}/projects/Jediverse/target/classes:." \
+./target/classes:." \
 com.pla.jediverse.CommandLineInterface
 
